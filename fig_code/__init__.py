1175516fcb99d722e7122b9bdefb20f6bcf6d14f
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-04-02 10:16:43 +0000

    Update travis file.

--- fig_code/__init__.py
+++ fig_code/__init__.py
@@ -2,4 +2,6 @@ from .data import *
 from .figures import *
 
 from .sgd_separator import plot_sgd_separator
-from .linear_regression imp
+from .linear_regression import plot_linear_regression
+from .kmeans import plot_kmeans
+from .helpers import plot_iris_knn
