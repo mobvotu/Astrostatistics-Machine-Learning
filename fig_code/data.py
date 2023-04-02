460d2504ce3974f35e124253cd039b5da1dc4069
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-04-02 18:27:25 +0000

    mm: compaction: detect when scanners meet in isolate_freepages

--- fig_code/data.py
+++ fig_code/data.py
@@ -5,4 +5,11 @@ def linear_data_sample(N=40, rseed=0, m=3, b=-2):
     rng = np.random.RandomState(rseed)
 
     x = 10 * rng.rand(N)
-    dy = m / 2 * (1 + rng
+    dy = m / 2 * (1 + rng.rand(N))
+    y = m * x + b + dy * rng.randn(N)
+
+    return (x, y, dy)
+
+
+def linear_data_sample_big_errs(N=40, rseed=0, m=3, b=-2):
+    rng 
