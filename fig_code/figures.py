b556cd154033888321c98f41dd31da1d57c0bfdd
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-04-13 07:59:50 +0000

    Update IDxyz.js

--- fig_code/figures.py
+++ fig_code/figures.py
@@ -4,4 +4,6 @@ import warnings
 
 
 def plot_venn_diagram():
-    fig, ax = plt.subplots(subplot_kw=dic
+    fig, ax = plt.subplots(subplot_kw=dict(frameon=False, xticks=[], yticks=[]))
+    ax.add_patch(plt.Circle((0.3, 0.3), 0.3, fc='red', alpha=0.5))
+    ax.add_pa
