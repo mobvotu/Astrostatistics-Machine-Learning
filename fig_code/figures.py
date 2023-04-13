c683917ffce03828dc47f5011bf4eee73cecf52f
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-04-13 16:48:31 +0000

    More README updates

--- fig_code/figures.py
+++ fig_code/figures.py
@@ -8,4 +8,6 @@ def plot_venn_diagram():
     ax.add_patch(plt.Circle((0.3, 0.3), 0.3, fc='red', alpha=0.5))
     ax.add_patch(plt.Circle((0.6, 0.3), 0.3, fc='blue', alpha=0.5))
     ax.add_patch(plt.Rectangle((-0.1, -0.1), 1.1, 0.8, fc='none', ec='black'))
-    ax.text(0.2, 
+    ax.text(0.2, 0.3, '$x$', size=30, ha='center', va='center')
+    ax.text(0.7, 0.3, '$y$', size=30, ha='center', va='center')
+    ax.text(0.0, 0.6, '$I$', size
