cce4044fbc881237a6e7a2b27539c6bdc0697e85
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-04-14 07:29:34 +0000

    Improve settings

--- fig_code/figures.py
+++ fig_code/figures.py
@@ -10,4 +10,10 @@ def plot_venn_diagram():
     ax.add_patch(plt.Rectangle((-0.1, -0.1), 1.1, 0.8, fc='none', ec='black'))
     ax.text(0.2, 0.3, '$x$', size=30, ha='center', va='center')
     ax.text(0.7, 0.3, '$y$', size=30, ha='center', va='center')
-    ax.text(0.0, 0.6, '$I$', size
+    ax.text(0.0, 0.6, '$I$', size=30)
+    ax.axis('equal')
+
+
+def plot_example_decision_tree():
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_axes([0, 0, 0.8, 1], frameon=False
