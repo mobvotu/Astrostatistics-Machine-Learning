065eeddd53da890102b87033336032a9af46691b
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-06-29 08:41:24 +0000

    Final Project

--- fig_code/helpers.py
+++ fig_code/helpers.py
@@ -4,4 +4,8 @@ Small helpers for code that is not shown in the notebooks
 
 from sklearn import neighbors, datasets, linear_model
 import pylab as pl
-import numpy as
+import numpy as np
+from matplotlib.colors import ListedColormap
+
+# Create color maps for 3-class classification problem, as with iris
+cmap_lig
