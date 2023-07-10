66c3de8811c1a285e85fbbe4369792e09c8f4b1e
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-07-10 18:20:15 +0000

    kbuild: drop cpuinit/cpuexit checks in modpost

--- fig_code/helpers.py
+++ fig_code/helpers.py
@@ -42,4 +42,12 @@ def plot_polynomial_regression():
     rng = np.random.RandomState(0)
     x = 2*rng.rand(100) - 1
 
-    f = lambda t: 1.2 * t**2 + .1 * t**3 - .4 * t **5 - .5 * t ** 
+    f = lambda t: 1.2 * t**2 + .1 * t**3 - .4 * t **5 - .5 * t ** 9
+    y = f(x) + .4 * rng.normal(size=100)
+
+    x_test = np.linspace(-1, 1, 100)
+
+    pl.figure()
+    pl.scatter(x, y, s=4)
+
+    X
