155bff9e8f19b4122931a15773b3c2ef6f959346
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-07-13 20:34:45 +0000

    updated pom

--- fig_code/helpers.py
+++ fig_code/helpers.py
@@ -52,4 +52,9 @@ def plot_polynomial_regression():
 
     X = np.array([x**i for i in range(5)]).T
     X_test = np.array([x_test**i for i in range(5)]).T
-    regr = linear_model.LinearRegressi
+    regr = linear_model.LinearRegression()
+    regr.fit(X, y)
+    pl.plot(x_test, regr.predict(X_test), label='4th order')
+
+    X = np.array([x**i for i in range(10)]).T
+    X_test
