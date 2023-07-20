f2d9420ab8fa6fdd3216b4bd98d213f31fbd903c
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-07-20 17:30:33 +0000

    Added acknowledgements

--- fig_code/kmeans.py
+++ fig_code/kmeans.py
@@ -6,4 +6,10 @@ from sklearn.cluster import KMeans
 
 def plot_kmeans():
     X, y = make_blobs(n_samples=300, centers=4,
-                      r
+                      random_state=0, cluster_std=0.60)
+
+    y_pred = KMeans(4).fit(X).predict(X)
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+    ax[0].scatter(X[:
