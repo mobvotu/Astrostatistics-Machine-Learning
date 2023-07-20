e77610703c22377ef4fa5d2e33d75a2341c8eeec
mobvotu <142726384+mobvotu@users.noreply.github.com>
2023-07-20 13:01:28 +0000

    Update npm dependencies.

--- fig_code/kmeans.py
+++ fig_code/kmeans.py
@@ -1,2 +1,9 @@
 import matplotlib.pyplot as plt
-from sklearn.datasets.samples_generator import mak
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.cluster import KMeans
+
+
+
+def plot_kmeans():
+    X, y = make_blobs(n_samples=300, centers=4,
+                      r
